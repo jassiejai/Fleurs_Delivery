07a83539296536f9bfd5a72a6c6d8e80f57f90f1
Jassie <jasminehart94@gmail.com>
2022-01-31 10:05:24 -0800

    linked spring

---
+++ src/main/java/com/example/fleursdelivery/FleursDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.fleursdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleursDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FleursDeliveryApplication.class, args);
+    }
+
+}
