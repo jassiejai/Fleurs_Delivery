626bfaec148046ea2388ba1d648c0e7f93d17f6c
Jassie <jasminehart94@gmail.com>
2022-02-04 01:26:22 -0800

    able to update the flower option

--- src/main/java/com/example/fleursdelivery/model/Order.java
+++ src/main/java/com/example/fleursdelivery/model/Order.java
@@ -35,7 +35,7 @@ public class Order {
     @JoinColumn(name = "customerId")
     private Customer customer;
 
-    @OneToMany(mappedBy = "orders", orphanRemoval = true)
+    @OneToMany(mappedBy = "orders")
     @LazyCollection(LazyCollectionOption.FALSE)
     private List<Flower> flowerList;
 
